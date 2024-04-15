b3b8b38c228027c402004c942ea36697c99e98a8
xuanwei <105519816+buai33@users.noreply.github.com>
2024-04-14 22:47:33 -0400

    Add files via upload

---
+++ query/adm_details.sql
@@ -0,0 +1,9 @@
+SET search_path TO mimiciii;
+DROP MATERIALIZED VIEW IF EXISTS adm_details CASCADE;
+CREATE MATERIALIZED VIEW adm_details as
+(
+    select p.subject_id, p.gender, p.dob, p.dod, hadm_id, admittime, dischtime, admission_type, insurance, marital_status, ethnicity, hospital_expire_flag, has_chartevents_data
+    from admissions adm
+    join patients p
+    on adm.subject_id = p.subject_id
+)
